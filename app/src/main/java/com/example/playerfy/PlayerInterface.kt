72c009ce377150cd96d105a85d1380f6a7885b88
Nadir2225 <nadirna2003@gmail.com>
2024-10-31 13:10:45 +0100

    developping the tabrow functionality

--- app/src/main/java/com/example/playerfy/PlayerInterface.kt
+++ app/src/main/java/com/example/playerfy/PlayerInterface.kt
@@ -1,32 +1,59 @@
 package com.example.playerfy
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun PlayerInterface(songs: List<Song>) {
+    var songName by remember { mutableStateOf("") }
     val state = rememberPagerState(pageCount = { songs.size })
     HorizontalPager(state = state) { index ->
+        val song = songs[index]
+        songName = song.name
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .background(songs[index].color)
+                .fillMaxHeight(0.5F)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = songs[index].name)
+            Image(painter = painterResource(song.img?:R.drawable.song_img),
+                null,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+            )
         }
     }
-    PlayerActions()
+    PlayerActions(songName)
 }
 
 @Composable
-fun PlayerActions() {
-    Text(text = "hehe")
+fun PlayerActions(name: String) {
+    Text(text = name)
 }
--- app/src/main/java/com/example/playerfy/song.kt
+++ app/src/main/java/com/example/playerfy/song.kt
@@ -3,7 +3,7 @@ package com.example.playerfy
 import androidx.compose.ui.graphics.Color
 
 data class Song (
-    var img: Int? = 0,
+    var img: Int? = null,
     var name: String,
     var min: Int = 0,
     var sec: Int = 0,
